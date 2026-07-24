08910fa0d2b3429da4a100554e3fa7a515e144b3
sayedkhaled205 <sayedkhaled205@gmail.com>
2026-07-24 23:28:52 +0300

    Move More menu outside scrollable CMS tabs

--- kidia-mobile-cms/admin/pages/cms-shell.php
+++ kidia-mobile-cms/admin/pages/cms-shell.php
@@ -14,14 +14,14 @@ defined( 'ABSPATH' ) || exit;
 				<span><?php echo esc_html( $tab['label'] ); ?></span>
 			</a>
 		<?php endforeach; ?>
-		<div class="kidia-cms-more">
-			<button type="button" aria-expanded="false"><span class="dashicons dashicons-screenoptions"></span><span><?php esc_html_e( 'More', 'kidia-mobile-cms' ); ?></span><span class="dashicons dashicons-arrow-down-alt2"></span></button>
-			<div class="kidia-cms-more__menu">
-				<?php foreach ( $more_tabs as $key => $tab ) : ?>
-					<a href="<?php echo esc_url( $tab['url'] ); ?>" class="<?php echo $key === $active_tab ? 'is-active' : ''; ?>"><span class="dashicons <?php echo esc_attr( $tab['icon'] ); ?>"></span><?php echo esc_html( $tab['label'] ); ?></a>
-				<?php endforeach; ?>
-			</div>
-		</div>
 	</nav>
+	<div class="kidia-cms-more">
+		<button type="button" aria-expanded="false"><span class="dashicons dashicons-screenoptions"></span><span><?php esc_html_e( 'More', 'kidia-mobile-cms' ); ?></span><span class="dashicons dashicons-arrow-down-alt2"></span></button>
+		<div class="kidia-cms-more__menu">
+			<?php foreach ( $more_tabs as $key => $tab ) : ?>
+				<a href="<?php echo esc_url( $tab['url'] ); ?>" class="<?php echo $key === $active_tab ? 'is-active' : ''; ?>"><span class="dashicons <?php echo esc_attr( $tab['icon'] ); ?>"></span><?php echo esc_html( $tab['label'] ); ?></a>
+			<?php endforeach; ?>
+		</div>
+	</div>
 	<a class="kidia-cms-setup-link <?php echo 'setup' === $active_tab ? 'is-active' : ''; ?>" href="<?php echo esc_url( admin_url( 'admin.php?page=kidia-mobile-setup' ) ); ?>"><span class="dashicons dashicons-admin-customizer"></span><?php esc_html_e( 'Setup & Themes', 'kidia-mobile-cms' ); ?></a>
 </div>
