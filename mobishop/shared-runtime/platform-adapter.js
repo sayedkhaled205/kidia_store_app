c8a334f34cd174eb7af77457b8bd1ce8ff0fdb44
sayedkhaled205 <sayedkhaled205@gmail.com>
2026-08-21 19:48:20 +0300

    Add shared MobiShop builder runtime contract

---
+++ mobishop/shared-runtime/platform-adapter.js
@@ -0,0 +1,33 @@
+(function (global) {
+	'use strict';
+
+	const requiredMethods = [
+		'bootstrap',
+		'loadScreen',
+		'saveScreen',
+		'navigate',
+		'uploadMedia',
+		'startBuild'
+	];
+
+	function assertPlatformAdapter(adapter) {
+		if (!adapter || typeof adapter !== 'object') {
+			throw new TypeError('MobiShop requires a platform adapter.');
+		}
+		requiredMethods.forEach(function (method) {
+			if (typeof adapter[method] !== 'function') {
+				throw new TypeError('MobiShop platform adapter is missing ' + method + '().');
+			}
+		});
+		if (!adapter.platform || typeof adapter.platform !== 'string') {
+			throw new TypeError('MobiShop platform adapter requires a platform name.');
+		}
+		return adapter;
+	}
+
+	global.MobiShopPlatformAdapter = Object.freeze({
+		requiredMethods: requiredMethods.slice(),
+		assert: assertPlatformAdapter
+	});
+})(window);
+
